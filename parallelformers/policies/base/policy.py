375d304952aba8a33a5828585e2a026638186fa0
Hyunwoong Ko <gusdnd852@naver.com>
2021-07-18 00:24:30 +0900

    Update policy.py

--- parallelformers/policies/base/policy.py
+++ parallelformers/policies/base/policy.py
@@ -21,15 +21,27 @@ from torch import nn
 
 @dataclass
 class Layer:
-    """
+    r"""
     Dataclass used to describe a layer in the policy object
 
     Attributes:
-        weight and bias (str): the names of the weight and bias tensors, respectively. You can use the syntax such as [ ] or . to the tensor names. `.` is used as accessors in common programming languages and `[ ]` is used to access elements in nn.ModuleList.
-        n_fused (int): the number of areas used in fused layers. For example, GPT2 and TransfoXL have fused attention layers that consist of query, key and value. These layers should not be simply chunked by the number of GPUs. Instead, they should be divided into the query, key and value areas first.
-        replace (Any): the layer that you want to replace an existing layer with. The parallelization process by the tensor slicing method involves All-Reduce operations to collect tensors from all GPUs. So, we need to insert some layer like AllReduceLinear to replace the existing nn.Linear layer.
-        reversed (bool): this attribute is used to indicate whether tensors are reversed or not. Some models such as GPT1 and GPT2 use the transformers.modeling_utils.Conv1D layer instead of the nn.Linear layer. These layers store weight and bias tensors reversed.
-        ignore_checker (bool): this attribute is used when you want to ignore errors in case the layers do not exist. Some models like Bert, Roberta and Electra have only encoder layers. but for Huggingface, these models are also designed to be able to used as decoders. In these models, some layers may or may not be created depending on the configuraions. In this case, you can use ignore_checker option to ignore errors even if the layers do not always exist.
+        weight and bias (str): the names of the weight and bias tensors, respectively. You can use the syntax
+            such as [ ] or . to the tensor names. `.` is used as accessors in common programming languages and `[ ]`
+            is used to access elements in nn.ModuleList.
+        n_fused (int): the number of areas used in fused layers. For example, GPT2 and TransfoXL have fused
+            attention layers that consist of query, key and value. These layers should not be simply chunked by
+            the number of GPUs. Instead, they should be divided into the query, key and value areas first.
+        replace (Any): the layer that you want to replace an existing layer with. The parallelization process
+            by the tensor slicing method involves All-Reduce operations to collect tensors from all GPUs.
+            So, we need to insert some layer like AllReduceLinear to replace the existing nn.Linear layer.
+        reversed (bool): this attribute is used to indicate whether tensors are reversed or not. Some models such as
+            GPT1 and GPT2 use the transformers.modeling_utils.Conv1D layer instead of the nn.Linear layer.
+            These layers store weight and bias tensors reversed.
+        ignore_checker (bool): this attribute is used when you want to ignore errors in case the layers do not exist.
+            Some models like Bert, Roberta and Electra have only encoder layers. but for Huggingface,
+            these models are also designed to be able to used as decoders. In these models,
+            some layers may or may not be created depending on the configuraions.
+            In this case, you can use ignore_checker option to ignore errors even if the layers do not always exist.
     """
 
     weight: str = None
@@ -68,13 +80,12 @@ class Policy(ABC):
         Notes:
             The format of the dictionary object is as follows.
 
-            {
+            dict:
                 "param_name_1": reset_value_1,
                 "param_name_2": reset_value_2,
                 "param_name_3": reset_value_3,
                 ...
-                "param_name_n": reset_value_n,
-            }
+                "param_name_n": reset_value_n
         """
         return {}
 
@@ -89,13 +100,12 @@ class Policy(ABC):
         Notes:
             The format of the dictionary object is as follows.
 
-            {
+            dict:
                 orig_class_name_1: reset_module_class_1,
                 orig_class_name_2: reset_module_class_2,
                 orig_class_name_3: reset_module_class_3,
                 ...
-                orig_class_name_4: reset_module_class_n,
-            }
+                orig_class_name_4: reset_module_class_n
         """
         return {}
 
